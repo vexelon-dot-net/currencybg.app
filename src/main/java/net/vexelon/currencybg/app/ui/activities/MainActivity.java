2333857c5ebe39f42fc36925b1e63abb3a634d1f
Petar Petrov <petar.petrov.georgiev@gmail.com>
2016-02-21 17:15:05 +0100

    Replace tabs navigation with drawer

--- src/main/java/net/vexelon/currencybg/app/ui/activities/MainActivity.java
+++ src/main/java/net/vexelon/currencybg/app/ui/activities/MainActivity.java
@@ -18,21 +18,21 @@
 package net.vexelon.currencybg.app.ui.activities;
 
 import java.util.Calendar;
-import java.util.Locale;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.Fragment;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import net.vexelon.currencybg.app.Defs;
@@ -44,54 +44,27 @@ import net.vexelon.currencybg.app.ui.fragments.AbstractFragment;
 import net.vexelon.currencybg.app.ui.fragments.ConvertFragment;
 import net.vexelon.currencybg.app.ui.fragments.CurrenciesFragment;
 
-public class MainActivity extends AppCompatActivity implements ActionBar.TabListener, NotificationsListener {
+public class MainActivity extends AppCompatActivity implements NotificationsListener {
 
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a {@link FragmentPagerAdapter}
-	 * derivative, which will keep every
-	 * loaded fragment in memory. If this becomes too memory intensive, it
-	 * may be best to switch to a
-	 * {@link android.support.v13.app.FragmentStatePagerAdapter}.
-	 */
-	private SectionsPagerAdapter mSectionsPagerAdapter;
+	private DrawerLayout drawerLayout;
+	private NavigationView drawerNav;
+	private ActionBarDrawerToggle drawerToggle;
+	private Toolbar toolbar;
+	private Menu menu;
 	private PendingIntent pendingIntent;
-
-	private ViewPager mViewPager;
-	private Menu mMenu;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		final ActionBar actionBar = getSupportActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		actionBar.setDisplayShowTitleEnabled(true);
-		actionBar.setDisplayShowHomeEnabled(true);
+		toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections of the activity.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-
-		// When swiping between different sections, select the corresponding
-		// tab. We can also use ActionBar.Tab#select() to do this if we have
-		// a reference to the Tab.
-		mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-			@Override
-			public void onPageSelected(int position) {
-				actionBar.setSelectedNavigationItem(position);
-			}
-		});
-
-		// For each of the sections in the app, add a tab to the action bar.
-		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-			actionBar.addTab(actionBar.newTab().setText(mSectionsPagerAdapter.getPageTitle(i)).setTabListener(this));
-		}
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		drawerNav = (NavigationView) findViewById(R.id.navView);
+		setupDrawerContent(drawerNav);
+		drawerToggle = setupDrawerToggle();
 
 		// load default values
 		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
@@ -100,6 +73,107 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
 		startService();
 	}
 
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		drawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		drawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		this.menu = menu;
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (drawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		switch(item.getItemId()) {
+			case android.R.id.home:
+				drawerLayout.openDrawer(GravityCompat.START);
+				break;
+			case R.id.action_settings:
+				Intent intent = new Intent(this, PrefsActivity.class);
+				startActivity(intent);
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private ActionBarDrawerToggle setupDrawerToggle() {
+		return new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open,  R.string.drawer_close);
+	}
+
+	private void setupDrawerContent(NavigationView navigationView) {
+		navigationView.setNavigationItemSelectedListener(
+				new NavigationView.OnNavigationItemSelectedListener() {
+					@Override
+					public boolean onNavigationItemSelected(MenuItem menuItem) {
+						selectDrawerItem(menuItem, getClassFromMenu(menuItem));
+						return true;
+					}
+				});
+	}
+
+	private Class getClassFromMenu(MenuItem menuItem) {
+		switch(menuItem.getItemId()) {
+			case R.id.nav_convert:
+				return ConvertFragment.class;
+			case R.id.nav_currencies:
+			default:
+				return CurrenciesFragment.class;
+		}
+	}
+
+	/**
+	 * Create a new fragment and specify the planet to show based on position.
+	 *
+	 * @param menuItem
+	 */
+	public <T extends AbstractFragment> void selectDrawerItem(MenuItem menuItem, Class<T> clazz) {
+		AbstractFragment fragment = null;
+		try {
+			fragment = clazz.newInstance();
+			fragment.addListener(MainActivity.this);
+//			Bundle args = new Bundle();
+//			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+//			fragment.setArguments(args);
+
+			// Insert the fragment by replacing any existing fragment
+			FragmentManager fragmentManager = getSupportFragmentManager();
+			fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+			// Highlight the selected item, update the title, and close the drawer
+			menuItem.setChecked(true);
+			setTitle(menuItem.getTitle());
+			drawerLayout.closeDrawers();
+		} catch (Exception e) {
+			Log.e(Defs.LOG_TAG, "Unknown drawer section!", e);
+		}
+	}
+
+	@Override
+	public void onNotification(Notifications event) {
+		switch (event) {
+		case UPDATE_RATES_DONE:
+			// setRefreshActionButtonState(false);
+			break;
+		}
+	}
+
+	/**
+	 * Starts background currencies update service
+	 *
+	 */
 	public void startService() {
 		Intent myIntent = new Intent(MainActivity.this, RateService.class);
 		pendingIntent = PendingIntent.getService(MainActivity.this, 0, myIntent, 0);
@@ -115,133 +189,6 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
 	public void cancelService() {
 		AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 		alarmManager.cancel(pendingIntent);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		mMenu = menu;
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			Intent intent = new Intent(this, PrefsActivity.class);
-			startActivity(intent);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	@Override
-	public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-		// When the given tab is selected, switch to the corresponding page in
-		// the ViewPager.
-		mViewPager.setCurrentItem(tab.getPosition());
-	}
-
-	@Override
-	public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-	}
-
-	@Override
-	public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-	}
-
-	@Override
-	public void onNotification(Notifications event) {
-		switch (event) {
-		case UPDATE_RATES_DONE:
-			// setRefreshActionButtonState(false);
-			break;
-		}
-
-	}
-
-	private Fragment getCurrentFragment() {
-		String fragmentName = "android:switcher:" + mViewPager.getId() + ":" + mViewPager.getCurrentItem();
-		return getSupportFragmentManager().findFragmentByTag(fragmentName);
-	}
-
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fragmentManager) {
-			super(fragmentManager);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a PlaceholderFragment (defined as a static inner class
-			// below).
-			Fragment fragment = PlaceholderFragment.newInstance(position + 1);
-			((AbstractFragment) fragment).addListener(MainActivity.this);
-			return fragment;
-		}
-
-		@Override
-		public int getCount() {
-			// Show 2 total pages.
-			return 2;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.tab_title_currencies).toUpperCase(l);
-			case 1:
-				return getString(R.string.tab_title_convert).toUpperCase(l);
-			}
-			return null;
-		}
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		private static final String ARG_SECTION_NUMBER = "section_number";
-
-		/**
-		 * Returns a new instance of this fragment for the given section
-		 * number.
-		 */
-		public static Fragment newInstance(int sectionNumber) {
-			AbstractFragment fragment = null;
-			switch (sectionNumber) {
-			case 1:
-				fragment = new CurrenciesFragment();
-				break;
-			case 2:
-				fragment = new ConvertFragment();
-				break;
-			}
-			if (fragment != null) {
-				Bundle args = new Bundle();
-				args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-				fragment.setArguments(args);
-			}
-			return fragment;
-		}
-
 	}
 
 }
