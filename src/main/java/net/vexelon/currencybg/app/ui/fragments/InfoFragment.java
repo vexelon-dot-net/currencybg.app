5584372ccee20d9239cfcd31de86ff8770b8215e
Petar Petrov <petar.petrov.georgiev@gmail.com>
2016-05-23 18:42:29 +0200

    Update info view

--- src/main/java/net/vexelon/currencybg/app/ui/fragments/InfoFragment.java
+++ src/main/java/net/vexelon/currencybg/app/ui/fragments/InfoFragment.java
@@ -41,69 +41,70 @@ import java.util.List;
 
 public class InfoFragment extends AbstractFragment {
 
-	private static final String URL_3RDPARTY = "intr://3rd";
+    private static final String URL_3RDPARTY = "intr://3rd";
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		this.rootView = inflater.inflate(R.layout.fragment_info, container, false);
-		init(rootView);
-		return rootView;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        this.rootView = inflater.inflate(R.layout.fragment_info, container, false);
+        init(rootView);
+        return rootView;
+    }
 
-	private void init(View view) {
-		final Activity activity = getActivity();
-		ListView lvInfo = (ListView) view.findViewById(R.id.list_info);
-		final InfoListAdapter adapter = new InfoListAdapter(getActivity(), R.layout.info_row, getInfosList());
-		lvInfo.setAdapter(adapter);
-		lvInfo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				String url = adapter.getUrl(position);
-				if (URL_3RDPARTY.equals(url)) {
-					new MaterialDialog.Builder(getActivity()).customView(R.layout.fragment_thirdparty, true)
-							.positiveText(R.string.text_ok).build().show();
-				} else if (url != null) {
-					Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-					startActivity(browserIntent);
-				}
-			}
-		});
-	}
+    private void init(View view) {
+        final Activity activity = getActivity();
+        ListView lvInfo = (ListView) view.findViewById(R.id.list_info);
+        final InfoListAdapter adapter = new InfoListAdapter(getActivity(), R.layout.info_row, getInfosList());
+        lvInfo.setAdapter(adapter);
+        lvInfo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String url = adapter.getUrl(position);
+                if (URL_3RDPARTY.equals(url)) {
+                    new MaterialDialog.Builder(getActivity()).customView(R.layout.fragment_thirdparty, true)
+                            .positiveText(R.string.text_ok).build().show();
+                } else if (url != null) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(browserIntent);
+                }
+            }
+        });
+    }
 
-	private List<InfoListAdapter.InfoItem> getInfosList() {
-		List<InfoListAdapter.InfoItem> infoList = Lists.newArrayList();
+    private List<InfoListAdapter.InfoItem> getInfosList() {
+        List<InfoListAdapter.InfoItem> infoList = Lists.newArrayList();
 
-		PackageInfo packageInfo = null;
-		try {
-			packageInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(),
-					PackageManager.GET_GIDS);
-			infoList.add(newInfoRow(getString(R.string.about_version), packageInfo.versionName));
-		} catch (Exception e) {
-			Log.e(Defs.LOG_TAG, "", e);
-		}
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(),
+                    PackageManager.GET_GIDS);
+            infoList.add(newInfoRow(getString(R.string.about_version), packageInfo.versionName));
+        } catch (Exception e) {
+            Log.e(Defs.LOG_TAG, "", e);
+        }
 
-		infoList.add(newInfoRow(getString(R.string.about_rates_provider), getString(R.string.about_rates_provider_text),
-				"http://www.bnb.bg"));
-		infoList.add(newInfoRow(getString(R.string.about_join_appdev), "github.com/vexelon-dot-net/currencybg.app",
-				"https://github.com/vexelon-dot-net/currencybg.app"));
-		infoList.add(newInfoRow(getString(R.string.about_author), getString(R.string.about_author_text)));
-		infoList.add(newInfoRow(getString(R.string.about_logo), getString(R.string.about_logo_text),
-				"http://www.stremena.com"));
-		infoList.add(newInfoRow(getString(R.string.about_flag_icons),
-				"Copyright (c) 2013 Aha-Soft. http://www.aha-soft.com/free-icons/free-yellow-button-icons",
-				"http://www.aha-soft.com/free-icons/free-yellow-button-icons"));
-		infoList.add(newInfoRow(getString(R.string.about_flag_icons),
-				"Copyright (CC BY-ND 3.0) Visual Pharm. http://icons8.com", "http://icons8.com"));
-		infoList.add(newInfoRow(getString(R.string.about_3rdparty), "", URL_3RDPARTY));
+        infoList.add(newInfoRow(getString(R.string.about_rates_provider), getString(R.string.about_rates_provider_text),
+                "http://www.bnb.bg"));
+        infoList.add(newInfoRow(getString(R.string.about_join_appdev), "github.com/vexelon-dot-net/currencybg.app",
+                "https://github.com/vexelon-dot-net/currencybg.app"));
+        infoList.add(newInfoRow(getString(R.string.about_author), getString(R.string.about_author_text),
+                "https://github.com/vexelon-dot-net/currencybg.app/blob/master/CREDITS"));
+        infoList.add(newInfoRow(getString(R.string.about_logo), getString(R.string.about_logo_text),
+                "http://www.stremena.com"));
+        infoList.add(newInfoRow(getString(R.string.about_flag_icons),
+                "Copyright (c) 2013 Aha-Soft. http://www.aha-soft.com/free-icons/free-yellow-button-icons",
+                "http://www.aha-soft.com/free-icons/free-yellow-button-icons"));
+        infoList.add(newInfoRow(getString(R.string.about_flag_icons),
+                "Copyright (CC BY-ND 3.0) Visual Pharm. http://icons8.com", "http://icons8.com"));
+        infoList.add(newInfoRow(getString(R.string.about_3rdparty), "", URL_3RDPARTY));
 
-		return infoList;
-	}
+        return infoList;
+    }
 
-	private InfoListAdapter.InfoItem newInfoRow(String name, String value, String url) {
-		return new InfoListAdapter.InfoItem(name, value, url);
-	}
+    private InfoListAdapter.InfoItem newInfoRow(String name, String value, String url) {
+        return new InfoListAdapter.InfoItem(name, value, url);
+    }
 
-	private InfoListAdapter.InfoItem newInfoRow(String name, String value) {
-		return newInfoRow(name, value, null);
-	}
+    private InfoListAdapter.InfoItem newInfoRow(String name, String value) {
+        return newInfoRow(name, value, null);
+    }
 }
