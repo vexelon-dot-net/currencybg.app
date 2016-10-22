b16d4aac171b24f1731c583b2f9a69fd59b86576
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2016-10-22 18:04:26 +0300

    Remove unused code from CurrenciesSQLiteDB.java

--- src/main/java/net/vexelon/currencybg/app/db/CurrenciesSQLiteDB.java
+++ src/main/java/net/vexelon/currencybg/app/db/CurrenciesSQLiteDB.java
@@ -25,37 +25,6 @@ import android.util.Log;
 
 public class CurrenciesSQLiteDB extends SQLiteOpenHelper {
 
-	// // Table Create Statements
-	// // TABLE_CURRENCY table create statement
-	// private static final String CREATE_TABLE_CURRENCY = String.format(
-	// "create table %s(%s integer primary key autoincrement, %s integer not
-	// null, %s text not null, %s text not null, %s integer not null, %s text,
-	// %s text not null, %s text, %s text not null, %s text, %s integer not
-	// null, %s text);",
-	// Defs.TABLE_CURRENCY, Defs.COLUMN_ID, Defs.COLUMN_GOLD, Defs.COLUMN_NAME,
-	// Defs.COLUMN_CODE,
-	// Defs.COLUMN_RATIO, Defs.COLUMN_REVERSERATE, Defs.COLUMN_RATE,
-	// Defs.COLUMN_EXTRAINFO, Defs.COLUMN_CURR_DATE,
-	// Defs.COLUMN_TITLE, Defs.COLUMN_F_STAR, Defs.COLUMN_LOCALE);
-	//
-	// // TABLE_CURRENCY_DATE table create statement
-	// private static final String CREATE_TABLE_CURRENCY_DATE = String.format(
-	// "create table %s(%s integer primary key autoincrement, %s text not null,
-	// %s text not null);",
-	// Defs.TABLE_CURRENCY_DATE, Defs.COLUMN_ID, Defs.COLUMN_CURR_DATE,
-	// Defs.COLUMN_LOCALE);
-	//
-	// // CREATE_TABLE_FIXED_CURRENCY table create statement
-	// public static final String CREATE_TABLE_FIXED_CURRENCY = String.format(
-	// "create table %s(%s integer primary key autoincrement, %s integer not
-	// null, %s text not null, %s text not null, %s integer not null, %s text,
-	// %s text not null, %s text, %s text not null, %s text, %s integer not
-	// null, %s text);",
-	// Defs.TABLE_FIXED_CURRENCY, Defs.COLUMN_ID, Defs.COLUMN_GOLD,
-	// Defs.COLUMN_NAME, Defs.COLUMN_CODE,
-	// Defs.COLUMN_RATIO, Defs.COLUMN_REVERSERATE, Defs.COLUMN_RATE,
-	// Defs.COLUMN_EXTRAINFO, Defs.COLUMN_CURR_DATE,
-	// Defs.COLUMN_TITLE, Defs.COLUMN_F_STAR, Defs.COLUMN_LOCALE);
 
 	// Table Create Statements
 	// TABLE_CURRENCY table create statement
@@ -73,10 +42,6 @@ public class CurrenciesSQLiteDB extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase database) {
 		// creating required tables
-
-		// database.execSQL(CREATE_TABLE_CURRENCY);
-		// database.execSQL(CREATE_TABLE_CURRENCY_DATE);
-		// database.execSQL(CREATE_TABLE_FIXED_CURRENCY);
 
 		database.execSQL(CREATE_TABLE_CURRENCY_BG);
 	}
