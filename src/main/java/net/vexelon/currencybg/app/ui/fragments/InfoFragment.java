2e2e85270ddf9d40c5351b8f211b44da1aa5eefa
Petar Petrov <petar.petrov.georgiev@gmail.com>
2017-11-01 18:52:49 +0100

    Fix translations & links on Info fragment

--- src/main/java/net/vexelon/currencybg/app/ui/fragments/InfoFragment.java
+++ src/main/java/net/vexelon/currencybg/app/ui/fragments/InfoFragment.java
@@ -103,14 +103,14 @@ public class InfoFragment extends AbstractFragment {
 
 		infoList.add(newInfoRow(getString(R.string.about_license), getString(R.string.copyright),
 				"https://raw.githubusercontent.com/vexelon-dot-net/currencybg.app/master/LICENSE"));
-		infoList.add(newInfoRow(getString(R.string.about_issue_appdev), getString(R.string.about_issue_appdev_text),
-				"https://github.com/vexelon-dot-net/currencybg.app/issues"));
-		infoList.add(newInfoRow(getString(R.string.about_join_appdev_authors), getString(R.string.about_join_appdev),
-				"https://github.com/vexelon-dot-net/currencybg.app/blob/master/CREDITS"));
 		infoList.add(newInfoRow(getString(R.string.about_fb), "facebook.com/currencybg",
 				"https://www.facebook.com/currencybg"));
 		infoList.add(
 				newInfoRow(getString(R.string.about_twitter), "twitter.com/vexelon", "https://twitter.com/vexelon"));
+		infoList.add(newInfoRow(getString(R.string.about_issue_appdev), getString(R.string.about_issue_appdev_text),
+				"https://github.com/vexelon-dot-net/currencybg.app/issues"));
+		infoList.add(newInfoRow(getString(R.string.about_join_appdev_authors), getString(R.string.about_join_appdev),
+				"https://github.com/vexelon-dot-net/currencybg.app"));
 		infoList.add(newInfoRow(getString(R.string.about_logo), getString(R.string.about_logo_text),
 				"http://www.stremena.com"));
 		infoList.add(newInfoRow(getString(R.string.about_3rdparty_icons), "", URL_3RDPARTY_ICONS));
