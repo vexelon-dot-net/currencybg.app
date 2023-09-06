5313fd4f700e1123d4c1e9499413e8a1d0a4547d
Petar Petrov <ppetrov@vexelon.net>
2023-09-06 21:21:34 +0200

    Mark deprecated sources and bump gradle version

--- src/main/java/net/vexelon/currencybg/app/common/Sources.java
+++ src/main/java/net/vexelon/currencybg/app/common/Sources.java
@@ -32,110 +32,109 @@ import java.util.Deque;
  * {@code currency_sources_ids} array in {@code arrays.xml}.
  */
 public enum Sources {
-	BNB(1, false), // unused
-	FIB(100),
-	TAVEX(200),
-	POLANA1(300),
-	FACTORIN(400),
-	UNICREDIT(500),
-	SGEB(600),
-	CRYPTO(700),
-	CHANGEPARTNER(800),
-	FOREXHOUSE(900),
-	ALLIANZ(1000),
-	CRYPTOBANK(1100, false), // deprecated
-	BITCOINSHOUSE(1200),
-	XCHANGE(1300),
-	ALTCOINS(1400);
+    BNB(1, false), // unused
+    FIB(100),
+    TAVEX(200),
+    POLANA1(300),
+    FACTORIN(400),
+    UNICREDIT(500),
+    SGEB(600, false), // deprecated
+    CRYPTO(700),
+    CHANGEPARTNER(800),
+    FOREXHOUSE(900, false), // deprecated
+    ALLIANZ(1000),
+    CRYPTOBANK(1100, false), // deprecated
+    BITCOINSHOUSE(1200, false), // deprecated
+    XCHANGE(1300),
+    ALTCOINS(1400);
 
-	private int id;
-	private boolean enabled;
+    private int id;
+    private boolean enabled;
 
-	Sources(int id, boolean enabled) {
-		this.id = id;
-		this.enabled = enabled;
-	}
+    Sources(int id, boolean enabled) {
+        this.id = id;
+        this.enabled = enabled;
+    }
 
-	Sources(int id) {
-		this(id, true);
-	}
+    Sources(int id) {
+        this(id, true);
+    }
 
-	public int getID() {
-		return id;
-	}
+    public int getID() {
+        return id;
+    }
 
-	public boolean isEnabled() {
-		return enabled;
-	}
+    public boolean isEnabled() {
+        return enabled;
+    }
 
-	public String getName(Context context) {
-		return getName(context, id);
-	}
+    public String getName(Context context) {
+        return getName(context, id);
+    }
 
-	public String getFullName(Context context) {
-		return getFullName(context, id);
-	}
+    public String getFullName(Context context) {
+        return getFullName(context, id);
+    }
 
-	public String getWebAddress(Context context) {
-		return getWebAddress(context, id);
-	}
+    public String getWebAddress(Context context) {
+        return getWebAddress(context, id);
+    }
 
-	/**
-	 *
-	 * @param id
-	 * @return {@link Sources} or {@code null}.
-	 */
-	public static Sources valueOf(int id) {
-		for (Sources s : Sources.values()) {
-			if (s.getID() == id) {
-				return s;
-			}
-		}
-		return null;
-	}
+    /**
+     * @param id
+     * @return {@link Sources} or {@code null}.
+     */
+    public static Sources valueOf(int id) {
+        for (Sources s : Sources.values()) {
+            if (s.getID() == id) {
+                return s;
+            }
+        }
+        return null;
+    }
 
-	public static Collection<Sources> getSorted() {
-		Deque<Sources> result = new ArrayDeque<>();
-		for (Sources source : Sources.values()) {
-			switch (source) {
-			case CRYPTO:
-			case CRYPTOBANK:
-			case BITCOINSHOUSE:
-			case XCHANGE:
-			case ALTCOINS:
-				result.push(source);
-				break;
-			default:
-				result.add(source);
-			}
-		}
-		return result;
-	}
+    public static Collection<Sources> getSorted() {
+        Deque<Sources> result = new ArrayDeque<>();
+        for (Sources source : Sources.values()) {
+            switch (source) {
+                case CRYPTO:
+                case CRYPTOBANK:
+                case BITCOINSHOUSE:
+                case XCHANGE:
+                case ALTCOINS:
+                    result.push(source);
+                    break;
+                default:
+                    result.add(source);
+            }
+        }
+        return result;
+    }
 
-	private static String getResource(Context context, int id, int resId) {
-		if (context != null) {
-			int[] sourceIds = context.getResources().getIntArray(R.array.currency_sources_ids);
-			String[] resources = context.getResources().getStringArray(resId);
+    private static String getResource(Context context, int id, int resId) {
+        if (context != null) {
+            int[] sourceIds = context.getResources().getIntArray(R.array.currency_sources_ids);
+            String[] resources = context.getResources().getStringArray(resId);
 
-			for (int i = 0; i < sourceIds.length; i++) {
-				if (sourceIds[i] == id) {
-					return resources[i];
-				}
-			}
-		}
+            for (int i = 0; i < sourceIds.length; i++) {
+                if (sourceIds[i] == id) {
+                    return resources[i];
+                }
+            }
+        }
 
-		return "";
-	}
+        return "";
+    }
 
-	public static String getName(Context context, int id) {
-		return getResource(context, id, R.array.currency_sources);
-	}
+    public static String getName(Context context, int id) {
+        return getResource(context, id, R.array.currency_sources);
+    }
 
-	public static String getFullName(Context context, int id) {
-		return getResource(context, id, R.array.currency_sources_full);
-	}
+    public static String getFullName(Context context, int id) {
+        return getResource(context, id, R.array.currency_sources_full);
+    }
 
-	public static String getWebAddress(Context context, int id) {
-		return getResource(context, id, R.array.currency_sources_web);
-	}
+    public static String getWebAddress(Context context, int id) {
+        return getResource(context, id, R.array.currency_sources_web);
+    }
 }
